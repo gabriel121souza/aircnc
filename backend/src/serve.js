532a7477fb2c8a84dda95fd066985c09e4c8decf
gabriel121souza <gabriel121souza@gmail.com>
2020-05-06 17:06:01 -0300

    frontend da aplicaçao semana omnistack 09

--- backend/src/serve.js
+++ backend/src/serve.js
@@ -2,6 +2,8 @@
  
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path')
 const routes = require('./routes');
 
 const app = express();
@@ -17,7 +19,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-sz2mf.mongodb.net/
 //req.params = Acessar route params (para edicao, delete)
 //req.body = Acessar corpo da requisicao (para criacao, edicao)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
